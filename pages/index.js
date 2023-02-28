e82044a8498c07fc5526a3b96aa855b76064d202
jyotiraditya <jyotiraditya24@gmail.com>
2023-02-28 11:56:03 +0530

    Programmtically Routing

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+  const router = useRouter();
+  function onHandleClick(){
+    console.log("order placed");
+    router.push('/product');
+  }
   return (
     <div>
       <h1>Home page</h1>
       <Link href="/blog">Click to here to view Blog</Link>
       <Link href="/product">Products</Link>
+      <button onClick={onHandleClick}>Place order</button>
     </div>
-    )
-
+  );
 }
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
-function ProductList(){
+
+function ProductList({productID = 100}){
     return (
-        <>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+      <>
+        <Link href="/product/1">Product 1</Link>
+        <Link href="/product/2">Product 2</Link>
+        <Link href="/product/3" replace>Product 3</Link>
+        <Link href={`/product/${productID}`}>Product query {productID}</Link>
         <Link href="/">Home</Link>
-       
-        </>
-    )
+      </>
+    );
 }
 
 export default ProductList;
