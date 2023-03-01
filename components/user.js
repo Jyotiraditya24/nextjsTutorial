f111158ad49b46bb176474cb7c9e10888b8c9000
jyotiraditya <jyotiraditya24@gmail.com>
2023-03-02 04:32:58 +0530

    learned about component

---
+++ components/user.js
@@ -0,0 +1,11 @@
+function User({ user }) { //or we can also user prop
+  return (
+    <>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <hr />
+    </>
+  );
+}
+
+export default User;
--- pages/users.js
+++ pages/users.js
@@ -1,35 +1,34 @@
-import React from 'react'
+import User from "@/components/user";
+import React from "react";
 
-//function users ({users}) or given below 
+//function users ({users}) or given below
 function users(props) {
   return (
     <div>
-        <h1>List of Users</h1>
-        <ul>
-            {props.users.map(user =>{
-                return (
-                    <div key={user.id}>
-                        <li>{user.name}</li>
-                        <li>{user.email}</li>
-                        <hr />
-                    </div>
-                )
-            })}
-        </ul>
+      <h1>List of Users</h1>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <div key={user.id}>
+              <User user={user} />
+            </div>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default users;
 
-export async function getStaticProps(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    console.log(data);
-    return {
-        props:{
-            users: data,
-        }
-    }
-    // returning an object
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      users: data,
+    },
+  };
+  // returning an object
 }
