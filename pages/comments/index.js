317f1fcc4d97df669a2b4fb6951f4d4bad5ec1c6
jyotiraditya <jyotiraditya24@gmail.com>
2023-03-28 22:16:23 +0530

    POST  request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,24 +2,40 @@ import { useState } from "react";
 
 function CommentsPage() {
   const [comments, setComments] = useState([{}]);
+  const [comment, setcomment] = useState("");
   async function fetchComments() {
     const response = await fetch("api/comments");
     const data = await response.json();
     setComments(data);
   }
+  const sumbitComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json(); 
+    console.log(data);
+  };
   return (
     <>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setcomment(e.target.value)}
+      />
+      <button onClick={sumbitComment}>Submit comment </button>
       <button onClick={fetchComments}>Load Comments</button>
-      {
-        comments.map((comment)=>{
-            return (
-                <div key={comment.id}>
-                    <p>{comment.id}</p>
-                    <p>{comment.text}</p>
-                </div>
-            )
-        })
-      }
+      {comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            <p>{comment.id}</p>
+            <p>{comment.text}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
